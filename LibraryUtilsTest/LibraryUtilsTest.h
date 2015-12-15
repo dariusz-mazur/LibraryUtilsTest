8055814ba102385d7d4c9038e38b8e124612b055
Dariusz Mazur <dariusz_mazur@MacBook-Pro-Dariusz.local>
2015-12-15 13:28:45 +0100

    First

---
+++ LibraryUtilsTest/LibraryUtilsTest.h
@@ -0,0 +1,19 @@
+//
+//  LibraryUtilsTest.h
+//  LibraryUtilsTest
+//
+//  Created by Dariusz Mazur on 15/12/2015.
+//  Copyright © 2015 Dariusz Mazur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibraryUtilsTest.
+FOUNDATION_EXPORT double LibraryUtilsTestVersionNumber;
+
+//! Project version string for LibraryUtilsTest.
+FOUNDATION_EXPORT const unsigned char LibraryUtilsTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryUtilsTest/PublicHeader.h>
+
+
---
+++ LibraryUtilsTest/VTestStatic.swift
@@ -0,0 +1,24 @@
+//
+//  VTestStatic.swift
+//  LibraryUtilsTest
+//
+//  Created by Dariusz Mazur on 15/12/2015.
+//  Copyright © 2015 Dariusz Mazur. All rights reserved.
+//
+
+import Foundation
+
+
+class VTestStatic
+{
+    internal static let STATIC_VAR = 534
+    
+    
+    internal class func someFunctionPrint(toPrint : String)
+    {
+        print("TEST: " + toPrint)
+    }
+    
+    
+    
+}
